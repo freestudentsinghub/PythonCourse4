84296df481c937ae42d177ec4c96e195bdcd8a86
freestudentsinghub <masha1207355@gmail.com>
2025-01-28 18:00:35 +0300

    add 1 model - clients

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
-# Create your views here.
+from clients.models import Clients
+
+
+class ClientsListView(ListView):
+    model = Clients
+    template_name = 'clients/clients_list.html'
+
+
+class HomeView(TemplateView):
+    template_name = "clients/home.html"
+
+
+class ClientsDetailView(DetailView):
+    model = Clients
+
+
+class ClientsDeleteView(DeleteView):
+    model = Clients
+    success_url = reverse_lazy('clients:clients_list')
+
+
+class ClientsUpdateView(UpdateView):
+    model = Clients
+    fields = ('email', 'full_name', 'comment',)
+    success_url = reverse_lazy('clients:clients_list')
+
+
+class ClientsCreateView(CreateView):
+    model = Clients
+    fields = ('email', 'full_name', 'comment',)
+    success_url = reverse_lazy('clients:clients_list')
